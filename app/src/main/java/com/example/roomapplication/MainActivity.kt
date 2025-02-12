ba6998450a143ae347935d53c9339babc97ded41
apagaSystems <apagasystems@gmail.com>
2025-02-12 18:47:58 +0400

    1st commit

--- app/src/main/java/com/example/roomapplication/MainActivity.kt
+++ app/src/main/java/com/example/roomapplication/MainActivity.kt
@@ -4,26 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -34,6 +34,7 @@ import androidx.compose.ui.graphics.Color
 
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 import com.example.roomapplication.db.Note
 import com.example.roomapplication.db.NoteDatabase
@@ -54,33 +55,31 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RoomApplicationTheme {
-//              CountAAAA()
-                
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CountAAAA()
                     Greeting(
                         modifier = Modifier.padding(innerPadding), noteDatabase
                     )
                 }
-
             }
         }
     }
 }
 
 
-//@Composable
-//fun CountAAAA(modifier: Modifier = Modifier, viewModel:CountViewModel= viewModel) {
-//
-//        val abc by viewModel.count.observeAsState(0)
-//
-//
-//    Column {
-//        Spacer(modifier = Modifier.height(48.dp))
-//
-//        Text(text = abc.toString())
-//        Text(text = "Inc", Modifier.clickable { viewModel.countInc() })
-//    }
-//}
+@Composable
+fun CountAAAA() {
+    val viewModel:CountViewModel= viewModel()
+        val abc by viewModel.count.observeAsState(0)
+
+
+    Column {
+        Spacer(modifier = Modifier.height(48.dp))
+
+        Text(text = abc.toString())
+        Text(text = "Inc", Modifier.clickable { viewModel.countInc() })
+    }
+}
 
 
 
@@ -89,7 +88,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier, database: NoteDatabase) {
-    var text by remember {
+    val text by remember {
 
         mutableStateOf("")
     }
@@ -104,11 +103,7 @@ fun Greeting(modifier: Modifier = Modifier, database: NoteDatabase) {
                 Color.Gray
             )
     ) {
-        val data by remember {
-            mutableStateOf(
-                setOf<String>()
-            )
-        }
+
         Text(text = text, modifier = modifier.padding(top = 32.dp))
 
         Button(onClick = {
